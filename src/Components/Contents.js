0598c81bbca4f4acd60de407942682edf03c2638
ssoa1111 <rishavallen@hanmail.net>
2022-05-26 19:49:27 +0900

    add annotate

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -8,7 +8,7 @@ const Contents = ({words}) => {
     const [score, setScore] = useState(0)
     let [countDown, setCountDown] = useState(0)
 
-
+    // input창 value 읽어오기
     const onInput = useCallback((e)=>{
         setInputText(e.target.value)
         if(words === e.target.value){
@@ -17,6 +17,7 @@ const Contents = ({words}) => {
         }
     },[])
     
+    // 시간제한 setinterval을 위한 useEffect
     useEffect(()=>{
         const countInterval = setInterval(()=>{
             countDown === 0 ? setStartgame(false) : setCountDown(--countDown)
@@ -25,12 +26,12 @@ const Contents = ({words}) => {
             if(!startgame){
                 clearInterval(countInterval)
             }
-            
+            // console.log에 countDown의 값이 0이 세번찍혀야 setStartgame이 false가 됩니다 왜 이런걸까요? 
         },1000)
         return ()=>clearInterval(countInterval)
     },[startgame])
     
-
+    // 버튼을 클릭하면 게임이 실행되게 하는 함수
     const onClick = useCallback(()=>{
         countDown = COUNT.current
         setCountDown(countDown)
@@ -46,6 +47,7 @@ const Contents = ({words}) => {
                 <p>남은시간 : <span>{countDown}</span>s</p>
                 <p>점수 : <span>{score}</span></p>
             </TextWrapper>
+            {/* startgame을 그대로 넣어도 안되어서 논리연산자를 넣어보라해서 했는데 안되네요.. */}
             <button onClick={onClick} startgame={startgame&&true}>{startgame? '게임 중...' : '게임 시작'}</button>
         </ContentsWrapper>
     );
